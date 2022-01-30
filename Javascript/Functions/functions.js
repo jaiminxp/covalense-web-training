ff9616325056118623585f08e987534476fd0d0b
Jaimin <jaimins100@gmail.com>
2022-01-30 16:49:31 +0530

    functions

---
+++ Javascript/Functions/functions.js
@@ -0,0 +1,29 @@
+// // function definition
+// function singSong(song) {
+//   console.log(song.toUpperCase());
+// }
+
+// function bankAccount(name, balance) {
+//   let penalty = 500;
+
+//   console.log(`Hi ${name} you balance is ${balance}`);
+//   return balance - penalty;
+// }
+
+// // running/using function
+// // singSong("baby by justin");
+
+// let myAccount = bankAccount("jayesh", 50000);
+
+// function dummy(num) {
+//   if (num > 5) {
+//     return 0;
+//   }
+
+//   console.log("DUM DUM");
+//   return 0;
+// }
+
+function cat() {
+  return ["meow", "purr"];
+}
